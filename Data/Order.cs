59fd44779821e9708b507b278d3f057fc2e61829
Emphyr00 <emphyr00@gmail.com>
2021-04-07 21:39:31 +0200

    Order fixes

--- Data/Order.cs
+++ Data/Order.cs
@@ -9,17 +9,20 @@ namespace Data
     public class Order
     {
         static List<OrderStatus> order_statuses = new List<OrderStatus>();
-        public Order(int ID, Buyer Buyer)
+        public Order(int ID, Buyer Buyer, IProduct Product)
         {
             this.ID = ID;
             this.Buyer = Buyer;
             this.CreatedAt = DateTime.Now;
             order_statuses.Add(new OrderStatus("Created", DateTime.Now));
+            this.Payment = new Payment();
+            this.Product = Product;
         }
-
+        public IProduct Product { get; set; }
+        public int Amount { get; set; }
         public int ID { get; set; }
         public Buyer Buyer { get; set; }
-
+        public Payment Payment { get; set; }
         public DateTime CreatedAt { get; set; }
 
         public OrderStatus GetLastStatus()
