b86be6c5b42c1959233f21e5e9fcf3a64db6e57b
230461 Lukasz Rembowski <230461@edu.p.lodz.pl>
2021-04-07 19:43:07 +0200

    Filled buyer and order classes

--- Data/Buyer.cs
+++ Data/Buyer.cs
@@ -7,6 +7,22 @@ using System.Threading.Tasks;
 namespace Data
 {
     public class Buyer
+
     {
+        public Buyer(string Name, string Surname, int ID, int PhoneNumber, int ItemCount)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.ID = ID;
+            Phone = PhoneNumber;
+            BoughtItems = ItemCount;
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int ID { get; set; }
+        public int Phone { get; set; }
+
+        public int BoughtItems { get; set; }
+
     }
 }
--- Data/Order.cs
+++ Data/Order.cs
@@ -8,5 +8,19 @@ namespace Data
 {
     class Order
     {
+        public Order(int ID, Buyer Buyer, DateTime Time)
+        {
+            this.ID = ID;
+            this.Buyer = Buyer;
+            this.Time = Time;
+            State = true;//true - ordered, false - cancelled
+        }
+
+        public int ID { get; set; }
+        public Buyer Buyer { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public bool State { get; set; }
     }
 }
