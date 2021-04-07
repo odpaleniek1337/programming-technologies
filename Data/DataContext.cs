24d19db75393ce12d3be89f0579a62722eb6c178
230461 Lukasz Rembowski <230461@edu.p.lodz.pl>
2021-04-07 20:44:12 +0200

    created interface Repository, DataRepository and DataContext classes

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    class DataContext
+    {
+        public Dictionary<int, IProduct> Products = new Dictionary<int, IProduct>();
+        public List<Buyer> Buyers = new List<Buyer>();
+        public List<Order> Orders = new List<Order>();
+    }
+}
---
+++ Data/DataRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    class DataRepository : IRepository
+    {
+        private DataContext context;
+
+        public DataRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public Dictionary<int, IProduct> GetProducts()
+        {
+            return context.Products;
+        }
+
+        public List<Buyer> GetBuyers()
+        {
+            return context.Buyers;
+        }
+
+        public List<Order> GetOrders()
+        {
+            return context.Orders;
+        }
+    }
+}
