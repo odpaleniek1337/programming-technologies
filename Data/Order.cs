d43b2bef7443133ed6281b8953de33223558df1b
Emphyr00 <emphyr00@gmail.com>
2021-04-11 12:38:01 +0200

    Order Status removed

--- Data/Order.cs
+++ Data/Order.cs
@@ -8,13 +8,11 @@ namespace Data
 {
     public class Order
     {
-        static List<OrderStatus> order_statuses = new List<OrderStatus>();
         public Order(int ID, Buyer Buyer, IProduct Product, int PaymentMethod)
         {
             this.ID = ID;
             this.Buyer = Buyer;
             this.CreatedAt = DateTime.Now;
-            order_statuses.Add(new OrderStatus("Created", DateTime.Now));
             this.Payment = new Payment(PaymentMethod, Product.Price);
             this.Product = Product;
         }
@@ -24,10 +22,5 @@ namespace Data
         public Buyer Buyer { get; set; }
         public Payment Payment { get; set; }
         public DateTime CreatedAt { get; set; }
-
-        public OrderStatus GetLastStatus()
-        {
-            return order_statuses[order_statuses.Count - 1];
-        }
     }
 }
