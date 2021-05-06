bfa05657a946076be6f58ab9bdb7e0914275cc6c
odpaleniek1337 <deadlodz@wp.pl>
2021-05-07 00:41:01 +0200

    added BuyerService

--- Task2/Logic/BuyerService.cs
+++ Task2/Logic/BuyerService.cs
@@ -2,11 +2,85 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Data;
 
-namespace Logic
+namespace Service
 {
     class BuyerService
     {
+        public IEnumerable<Buyer> GetBuyers()
+        {
+            using (var context = new ShopDataContext())
+            {
+                return context.Buyers.ToList();
+            }
+        }
+
+        public Buyer GetBuyer(string Phone)
+        {
+            using (var context = new ShopDataContext())
+            {
+                foreach (Buyer Buyer in context.Buyers.ToList())
+                {
+                    if (Buyer.phone.Equals(Phone))
+                    {
+                        return Buyer;
+                    }
+                }
+                return null;
+            }
+        }
+
+        public bool AddBuyer(string Name, string Surname, string Phone)
+        {
+            using (var context = new ShopDataContext())
+            {
+                if (GetBuyer(Phone) == null && !Name.Equals(null) && !Surname.Equals(null) && !Phone.Equals(null))
+                {
+                    Buyer NewBuyer = new Buyer
+                    {
+                        name = Name,
+                        surname = Surname,
+                        phone = Phone
+                    };
+                    context.Buyers.InsertOnSubmit(NewBuyer);
+                    context.SubmitChanges();
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public bool UpdateBuyer(int id, string Name, string Surname, string Phone)
+        {
+            using (var context = new ShopDataContext())
+            {
+                Buyer Buyer = context.Buyers.SingleOrDefault(i => i.id == id);
+                if (GetBuyer(Phone) == null && !Name.Equals(null) && !Surname.Equals(null) && !Phone.Equals(null))
+                {
+                    Buyer.name = Name;
+                    Buyer.surname = Surname;
+                    Buyer.phone = Phone;
+                    context.SubmitChanges();
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public bool DeleteBuyer(string Phone)
+        {
+            using (var context = new ShopDataContext())
+            {
+                Buyer Buyer = context.Buyers.SingleOrDefault(i => i.phone == Phone);
+                if (GetBuyer(Phone) != null && !Phone.Equals(null))
+                {
+                    context.Buyers.DeleteOnSubmit(Buyer);
+                    context.SubmitChanges();
+                    return true;
+                }
+                return false;
+            }
+        }
     }
 }
